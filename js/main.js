75f2293818343eda1ba65fc313aea35344171fad
Tatiana <tanushalti36@gmail.com>
2022-10-12 07:46:56 +0300

    готовое дз к третьему уроку

--- js/main.js
+++ js/main.js
@@ -1,23 +1,111 @@
-const products = [
-    {id: 1, title: 'Notebook', price: 2000, image: 'img/1.jpg'},
-    {id: 2, title: 'Mouse', price: 20, image: 'img/2.jpg'},
-    {id: 3, title: 'Keyboard', price: 200, image: 'img/3.jpg'},
-    {id: 4, title: 'Gamepad', price: 50, image: 'img/4.jpg'},
-];
-//Функция для формирования верстки каждого товара
-//Добавить в выводе изображение
-const renderProduct = (product) => {
-    return `<div class="product-item">
-                <img class="product-img" src="${product.image}">
-                <h3 class="product-title">${product.title}</h3>
-                <p class="product-price">${product.price} $</p>
-                <button class="buy-btn">Купить</button>
-            </div>`
-};
-const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item));
-    console.log(productsList);
-    document.querySelector('.products').innerHTML = productsList.join('');
-};
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-renderPage(products);
+
+
+    class ProductsList {
+    constructor(container = '.products'){
+        this.container = container;
+        this.goods = [];
+        this._getProducts()
+            .then(data => {
+                this.goods = data;
+                this.render()
+            });
+    }
+    _getProducts(){
+
+        return fetch(`${API}/catalogData.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            });
+
+    }
+    calcSum(){
+        return this.allProducts.reduce((accum, item) => accum += item.price, 0);
+    }
+    render(){
+        const block = document.querySelector(this.container);
+        for (let product of this.goods){
+            const productObj = new ProductItem(product);
+            block.insertAdjacentHTML('beforeend', productObj.render());
+        }
+
+    }
+}
+class ProductItem {
+    constructor(product, img = 'img/1.jpg'){
+        this.title = product.product_name;
+        this.price = product.price;
+        this.id = product.id_product;
+        this.img = img;
+    }
+    render(){
+        return `<div class="product-item" data-id="${this.id}">
+                <img src="${this.img}" alt="Some img">
+                <div class="desc">
+                    <h3>${this.title}</h3>
+                    <p>${this.price} $</p>
+                    <button class="buy-btn">Купить</button>
+                </div>
+            </div>`
+    }
+}
+
+let list = new ProductsList();
+
+class Basket {
+    constructor(container = '.basket') {
+        this.container = container;
+        this.goods = [];
+        this._clickBasket();
+        this._getBasketProduct()
+            .then(data => {
+                this.goods = data.contents;
+                this.render();
+            });
+    }
+    _clickBasket() {
+        document.querySelector(".btn-cart").addEventListener('click', () => {
+            document.querySelector(this.container).classList.toggle('hidden');
+        });
+    }
+    _getBasketProduct() {
+        return fetch(`${API}/getBasket.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            })
+    }
+    render() {
+        const blockBasket = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const objProduct =  new BasketProduct();
+
+            blockBasket.insertAdjacentHTML('beforeend', objProduct.render(product));
+
+        }
+    }
+}    
+
+class BasketProduct {
+    render (product) {
+        return `<div class="product_cart" data-id="${product.id_product}">
+        
+            <div class="product_info">
+            <p class="product_title">${product.product_name}</p>
+            <p class="product_quantity">Количество: ${product.quantity}</p>
+        <p class="product_price">Цена: $${product.price} </p>
+        </div>
+
+        <div class="lower_block">
+            <p class="total_product_price">Цена за ${product.quantity} товара $ ${product.quantity*product.price}</p>
+            <button class="del_btn" data-id="${product.id_product}">&times;</button>
+        </div>
+        </div>`
+
+
+    }
+}
+
+new Basket();
